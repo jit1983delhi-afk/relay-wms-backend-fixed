4edfa5b064b2c5adc02b30df0dbcff96050d4f4a
Jitender Pal Singh <jit1983delhi@gmail.com>
2025-11-05 01:23:33 +0530

    Fix DB connection and root path

--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
-import pkg from 'pg';
+import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
